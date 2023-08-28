c2d3d48b4dfa6968a01d93635f1d38dff9c56703
munizcrr0804 <55550903+munizcrr0804@users.noreply.github.com>
2023-08-28 18:03:50 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Cell_Phone_Inventory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.brandTextBox = new System.Windows.Forms.TextBox();
+            this.modelTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.phoneListBox = new System.Windows.Forms.ListBox();
+            this.addPhoneButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Cell Phone Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Brand:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Model:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(129, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Price:";
+            // 
+            // brandTextBox
+            // 
+            this.brandTextBox.Location = new System.Drawing.Point(172, 72);
+            this.brandTextBox.Name = "brandTextBox";
+            this.brandTextBox.Size = new System.Drawing.Size(101, 20);
+            this.brandTextBox.TabIndex = 4;
+            // 
+            // modelTextBox
+            // 
+            this.modelTextBox.Location = new System.Drawing.Point(173, 99);
+            this.modelTextBox.Name = "modelTextBox";
+            this.modelTextBox.Size = new System.Drawing.Size(100, 20);
+            this.modelTextBox.TabIndex = 5;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(173, 126);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.priceTextBox.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(396, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Select a Phone";
+            // 
+            // phoneListBox
+            // 
+            this.phoneListBox.FormattingEnabled = true;
+            this.phoneListBox.Location = new System.Drawing.Point(399, 48);
+            this.phoneListBox.Name = "phoneListBox";
+            this.phoneListBox.Size = new System.Drawing.Size(120, 95);
+            this.phoneListBox.TabIndex = 8;
+            // 
+            // addPhoneButton
+            // 
+            this.addPhoneButton.Location = new System.Drawing.Point(173, 170);
+            this.addPhoneButton.Name = "addPhoneButton";
+            this.addPhoneButton.Size = new System.Drawing.Size(75, 23);
+            this.addPhoneButton.TabIndex = 9;
+            this.addPhoneButton.Text = "Add Phone";
+            this.addPhoneButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(331, 211);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.addPhoneButton);
+            this.Controls.Add(this.phoneListBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.modelTextBox);
+            this.Controls.Add(this.brandTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox brandTextBox;
+        private System.Windows.Forms.TextBox modelTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox phoneListBox;
+        private System.Windows.Forms.Button addPhoneButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cell_Phone_Inventory
+{
+    public partial class Form1 : Form
+    {
+        // List to Hold CellPhone objects
+        List<CellPhone> phoneList = new List<CellPhone>();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // The GetPhoneData method
+        // as an argument It assigns the Data enetered by the 
+        // user to the objects properties
+        private void GetPhoneData(CellPhone)
+        {
+            // Temporary variable
+            decimal price;
+
+            // Get the phone's brand.
+            phone.Brand = brandTextBox.Text;
+
+            // get the phone's model.
+            phone.Model = modelTextBox.Text;
+
+            // Get the phone's price.
+            if (decimal.TryParse(priceTextBox.Text, out price))
+            {
+                phone.Price = price;
+            }
+            else
+            {
+                // display an error message.
+                MessageBox.Show("Invaild Price");
+            }
+        }
+
+        private void addPhoneButton_Click(object sender, EventArgs e)
+        {
+            // Create a CellPhone object.
+            CellPhone myPhone = new CellPhone();
+
+            // Get the phone data
+            GetPhoneData(myPhone);
+
+            // Add the CellPhone object to the list.
+            phoneList.Add(myPhone);
+
+            // Add an entry to the list box.
+            phoneListBox.Items.Add(myPhone.Brand + "" + myPhone.Model);
+
+            // Clear the textbox controls
+            brandTextBox.Clear();
+            modelTextBox.Clear();
+            priceTextBox.Clear();
+
+            // reset the focus.
+            brandTextBox.Focus();
+
+            private void phoneListBox_SelectedIndexChanged(object sender, EventArgs e)
+            {
+                // get the index of the selected item
+                int index = phoneListBox.SelectedIndex;
+
+                // display the selected items's price.
+                MessageBox.Show(phoneList[index].Price.ToString("c"));
+
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // close the form
+            this.Close();
+        }
+    }
+}
